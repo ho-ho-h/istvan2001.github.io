616c9547d4b8eb52ed365574980e516811a900e8
ho-ho-h <hollopisti550@gmail.com>
2021-10-18 09:30:17 +0200

    elefant

---
+++ elefant/alap.js
@@ -0,0 +1,16 @@
+fetch("https://raw.githubusercontent.com/siraly1636/siraly1636.github.io/main/elefant/assets/json/elefant.json")
+.then(x => x.json())
+.then(y => elefantJson(y));
+
+function elefantJson(adatok){
+    console.log(adatok);
+
+    var sz="";
+    for (elem of adatok){
+        sz+="<div class='col-sm-4 text-center box'><h4 class='name'>"+elem.name+"</h4>";
+        sz+="<a target='_blank' href='"+elem.image+"'><img src='"+elem.image+"' class='elefantkep'></a>";
+        sz+="<p class='link'><a target='_blank' href='"+elem.wikilink+"'>Wikipédia</a></p></div>" 
+
+    }
+    document.getElementById("elefantok").innerHTML=sz;
+}
